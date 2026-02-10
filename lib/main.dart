4af0f6bd5d243a255d060cde1eb09e60f40bf1f2
RanggaCikal <ranggacikal2@gmail.com>
2026-02-10 20:01:44 +0700

    remove unused main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:restorecommendapps/presentation/login/login_screen.dart';
 import 'package:restorecommendapps/theme/app_colors.dart';
-import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  // testFirestore();
   runApp(const MyApp());
-}
-
-void testFirestore() async {
-  await FirebaseFirestore.instance
-      .collection('connection_test')
-      .add({
-        'status': 'connected',
-        'time': Timestamp.now(),
-      });
 }
 
 class MyApp extends StatelessWidget {
